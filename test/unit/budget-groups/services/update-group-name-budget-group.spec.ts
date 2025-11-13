52e9927342d88cc729834fb9cb22970fe710bec5
venilson2 <venilson2009@hotmail.com>
2025-11-12 21:14:44 -0300

    feat(tests): add initial unit tests for budget group services

---
+++ test/unit/budget-groups/services/update-group-name-budget-group.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from '@jest/globals';
+
+describe('UpdateGroupNameBudgetGroupService', () => {
+  it('should be defined', () => {
+    expect(true).toBe(true);
+  });
+});
